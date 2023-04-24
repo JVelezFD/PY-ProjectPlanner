f1e9c84ff72610035a6668ec5f24dc959f85449b
JVelezFD <jvelez117@gmail.com>
2023-04-24 08:43:57 -0400

    added named tuples to each task broken down by title duration prerequisites

--- planner.py
+++ planner.py
@@ -1,4 +1,6 @@
 import csv;
+from collections import namedtuple;
+Task = namedtuple("Task", ["title", "duration", "prerequisites"] );
 
 def read_tasks(filename):
     tasks = {};
@@ -7,7 +9,7 @@ def read_tasks(filename):
         title = row[1];
         duration = float(row[2]);
         prerequisites = set(map(int,row[3].split()));
-        tasks[number] = (title, duration, \
+        tasks[number] = Task(title, duration, \
             prerequisites);
     return tasks;
 
